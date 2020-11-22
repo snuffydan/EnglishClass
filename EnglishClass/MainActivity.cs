8559258a9fa7718dcab829506e59236f816ba1da
snuffydan <danushikadan@gmail.com>
2020-11-22 20:02:58 +0530

    * MainActivity.cs: * EnglishClass.csproj: * logo.png: * Resource.designer.cs: * ic_launcher.png: * ic_launcher.png: * ic_launcher.png: * ic_launcher.png: * ic_launcher.png: * ic_launcher_round.png: * ic_launcher_round.png: * ic_launcher_round.png: * ic_launcher_round.png: * ic_launcher_round.png: * ic_launcher_foreground.png: * ic_launcher_foreground.png: * ic_launcher_foreground.png: * ic_launcher_foreground.png: * ic_launcher_foreground.png:
    * strings.xml: Chnaged app icon and other values

--- EnglishClass/MainActivity.cs
+++ EnglishClass/MainActivity.cs
@@ -9,7 +9,7 @@ using Android.Widget;
 
 namespace EnglishClass
 {
-    [Activity(Label = "@string/app_name", Theme = "@style/AppTheme.NoActionBar", MainLauncher = true)]
+    [Activity(Label = "@string/app_name", Theme = "@style/AppTheme.NoActionBar", Icon = "@drawable/logo", MainLauncher = true)]
     public class MainActivity : AppCompatActivity
     {
         protected override void OnCreate(Bundle savedInstanceState)
